d6e55847f1b383b0953800b7de00a9f60210f02a
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-15 23:05:18 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+end 
+
+fido = Person.new 
+
+fido.object_id
+
+class Person
+end 
+
+fido = Person.new 
+fido.methods 
+
+class Person
+  def walk
+    puts "The personis walking"
+  end
+end
