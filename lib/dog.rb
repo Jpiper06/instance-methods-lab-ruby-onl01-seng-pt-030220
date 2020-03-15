ff73351d962ef3ed460ee675d672df767fb46787
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-15 23:14:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,28 +11,4 @@ end
 class Dog 
 end 
 
-fido = Dog.new 
-
-fido.object_id
-
-class Dog
-end 
-
-fido = Dog.new 
-fido.methods 
-
-class Dog
-  def sit 
-    puts "The Dog is sitting"
-endobject_id
-
-class Dog
-end 
-
-fido = Dog.new 
-fido.methods 
-
-class Dog
-
-
 
